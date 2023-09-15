6d6c144c7215b0a765b208164f44027003880400
Gabriel Françon <gab.francon@gmail.com>
2023-09-15 13:22:07 +0200

    [Texturing] cleaned tex array creator script

--- Assets/Scripts/Tools/TextureArrayCreator.cs
+++ Assets/Scripts/Tools/TextureArrayCreator.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
+
 using System.Collections.Generic;
+using System.IO;
 
 namespace Tools
 {
@@ -13,9 +15,9 @@ namespace Tools
             ScriptableWizard.DisplayWizard<TextureArrayCreator>("Create Texture Array", "Build Asset");
         }
 
-        public string path = "Assets/Textures/";
+        public string saveFolder = "Assets/Textures/";
 
-        public string filename = "MyTextureArray";
+        public string fileName = "MyTextureArray";
 
         public List<Texture2D> textures = new List<Texture2D>();
 
@@ -23,32 +25,45 @@ namespace Tools
 
         void OnWizardCreate()
         {
-            _CompileArray(textures, path, filename);
+            CompileTextureArray(textures, saveFolder, fileName);
         }
 
-		private void _CompileArray(List<Texture2D> textures, string path, string filename)
-		{
-			if(textures == null || textures.Count == 0)
-			{
-				Debug.LogError("No textures assigned");
-				return;
-			}
+        private void CompileTextureArray(List<Texture2D> _textures, string _saveFolder, string _fileName)
+        {
+            if (_textures == null || _textures.Count == 0)
+            {
+                Debug.LogError("[TextureArrayCreator] No textures assigned.");
+                return;
+            }
 
-			Texture2D sample = textures[0];
-			Texture2DArray textureArray = new Texture2DArray(sample.width, sample.height, textures.Count, sample.format, false);
-			textureArray.filterMode = FilterMode.Trilinear;
-			textureArray.wrapMode = TextureWrapMode.Repeat;
+            // the first texture gives the size and format of the texture array, 
+            // and all other textures are supposed to share the same properties
+            Texture2D firstTex = _textures[0];
+            Texture2DArray textureArray = new Texture2DArray(firstTex.width, firstTex.height, _textures.Count, firstTex.format, false);
+            textureArray.filterMode = FilterMode.Trilinear;
+            textureArray.wrapMode = TextureWrapMode.Repeat;
 
-			for (int i = 0; i < textures.Count; i++)
-			{
-				Texture2D tex = textures[i];
-				textureArray.SetPixels(tex.GetPixels(0), i, 0);
-			}
-			textureArray.Apply();
-			
-			string uri = path + filename+".asset";
-			AssetDatabase.CreateAsset(textureArray, uri);
-			Debug.Log("Saved asset to " + uri);
-		}
+            // copies the textures in the CPU array
+            for (int i = 0; i < _textures.Count; i++)
+            {
+                Texture2D tex = _textures[i];
+
+                if (tex.width != textureArray.width || tex.height != textureArray.height || tex.format != textureArray.format)
+                {
+                    Debug.LogWarning("[TextureArrayCreator] Texture n°" + i + " skipped because its size and format are not compatible.");
+                    continue;
+                }
+
+                textureArray.SetPixels(tex.GetPixels(0), i, 0);
+            }
+
+            // uploads the texture array to GPU
+            textureArray.Apply();
+
+            // saves the asset on disk
+            string uri = Path.Combine(_saveFolder, _fileName) + ".asset";
+            AssetDatabase.CreateAsset(textureArray, uri);
+            Debug.Log("[TextureArrayCreator] Saved asset to " + uri + ".");
+        }
     }
 }
