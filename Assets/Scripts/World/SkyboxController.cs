eca0daa83c3d7c0280424927969f45427f3705a2
David Åsberg <47108520+davidasberg@users.noreply.github.com>
2023-10-13 11:41:39 +0200

    Remove day/night separation, unifiied day/night time and variables (#30)
    * Remove day/night separation, unifiied day/night time and variables
    * fix: fixed flashing bug again and improved visuals.
    * fix: sun is now set automatically as source
    
    ---------
    
    Co-authored-by: Anders Blomqvist <anders@minaemail.se>

--- Assets/Scripts/World/SkyboxController.cs
+++ Assets/Scripts/World/SkyboxController.cs
@@ -1,108 +1,108 @@
 using System;
 using UnityEngine;
 
+[ExecuteAlways]
 public class SkyboxController : MonoBehaviour
 {
     public Material skybox;
 
     public bool dayNightCycleEnabled = true;
 
-    [SerializeField, Range(1.0f, 20.0f)]
-    private float timeScale = 1.0f;
+    [SerializeField, Range(0.0f, 1.0f)]
+    [Tooltip("0.0f = midnight, 0.5f = noon, 1.0f = midnight")]
+    private float timeOfDay = 0.5f;
+
+    [SerializeField, Range(1.0f, 120f)]
+    [Tooltip("The number of seconds per day (realtime seconds)")]
+    private float timeScale = 30.0f;
+    public Gradient skyColor;
+    public Gradient fogColor;
 
     [Header("Sun Settings")]
 
     public Light sun;
-    public Gradient skyDayColor;
-    public Gradient fogDayColor;
-
     public float sunIntensityFactor = 1.5f;
-
     public int sunNoonTemperature = 6500;
     public int sunSetTemperature = 2000;
 
+    [Tooltip("The angle of the sun at noon (0 = straight up, 90 = on the horizon)")]
+    public float sunAngle = 20.0f;
+
+    [Tooltip("The path of the sun in the sky (0 = rises in the east, 90 = rises in the north)")]
+    public float sunPath = 0f;
+
     [Header("Moon Settings")]
-
     public Light moon;
-    public Gradient skyNightColor;
-    public Gradient fogNightColor;
-
     public float moonIntensityFactor = 1.5f;
 
-    [Header("Debug (do not edit)")]
+    private readonly float floatPrecision = 1000f;
 
-    [SerializeField] private float n_time;
-    [SerializeField] private bool day;
-    [SerializeField] private bool night;
-
-    [SerializeField] private float s_intensity;
-    [SerializeField] private float m_intensity;
+    private void Start()
+    {
+        RenderSettings.sun = sun;
+    }
 
     private void Update()
     {
-        if (!dayNightCycleEnabled)
+        float roundedTime = Mathf.Round(timeOfDay * floatPrecision) / floatPrecision;
+        skybox.SetColor("_SkyColor", skyColor.Evaluate(roundedTime));
+        skybox.SetColor("_FogColor", fogColor.Evaluate(roundedTime));
+        UpdateSun();
+        UpdateMoon();
+
+        if (!dayNightCycleEnabled || !Application.isPlaying)
         {
             return;
         }
 
-        UpdateSunAndMoon();
+        // timeScale = 60.0f means 1 minute per in-game day
+        // timeScale = 1.0f means 1 second per in-game day
+        timeOfDay += Time.deltaTime / timeScale;
 
-        if (day)
+        if (timeOfDay > 1.0f)
         {
-            n_time = sun.transform.rotation.eulerAngles.x / 90f;
-            n_time = Mathf.Clamp(n_time, 0, 1);
-            UpdateSkyDayColor();
-        }
-        else if (night)
-        {
-            n_time = moon.transform.rotation.eulerAngles.x / 90f;
-            n_time = Mathf.Clamp(n_time, 0, 1);
-            UpdateSkyNightColor();
+            timeOfDay -= 1.0f;
         }
     }
 
-    private void UpdateSunAndMoon()
+    private void UpdateSun()
     {
-        sun.transform.Rotate(Time.deltaTime * timeScale, 0, 0);
-        moon.transform.Rotate(Time.deltaTime * timeScale, 0, 0);
+        float rotation = (timeOfDay - 0.5f) * 360.0f + 90.0f;
+        sun.transform.rotation = Quaternion.Euler(rotation, sunPath, sunAngle);
+        
+        float angle = Vector3.Dot(sun.transform.forward, Vector3.down);
 
-        if (sun.transform.rotation.eulerAngles.x < 90)
+        float threshold = 0.15f;
+        if (angle < threshold && angle > 0.0f)
         {
-            day = true;
-            night = false;
-
-            float theta = sun.transform.rotation.eulerAngles.x;
-            s_intensity = Mathf.Sin(Mathf.Deg2Rad * theta) * sunIntensityFactor;
-            sun.intensity = s_intensity;
-
-            float temperature = sunSetTemperature + (sunNoonTemperature - sunSetTemperature) * s_intensity;
-            sun.colorTemperature = temperature;
+            float i = Mathf.Lerp(0.6f, sunIntensityFactor, angle / threshold);
+            sun.intensity = i;
+        }
+        else if (angle < 0.0f && angle > -threshold)
+        {
+            float i = Mathf.Lerp(0.6f, 0.0f, angle / -threshold);
+            sun.intensity = i;
+        }
+        else
+        {
+            if (angle < 0.0f)
+            {
+                sun.intensity = 0;
+            }
+            else
+            {
+                sun.intensity = sunIntensityFactor;
+            }   
         }
 
-        if (moon.transform.rotation.eulerAngles.x < 90)
-        {
-            day = false;
-            night = true;
-
-            float theta = moon.transform.rotation.eulerAngles.x;
-            m_intensity = Mathf.Clamp(Mathf.Sin(Mathf.Deg2Rad * theta), 0, 0.5f) * moonIntensityFactor;
-            moon.intensity = m_intensity;
-        }
+        sun.colorTemperature = Mathf.Lerp(sunSetTemperature, sunNoonTemperature, Mathf.Clamp01(angle));
     }
 
-    private void UpdateSkyDayColor()
+    private void UpdateMoon()
     {
-        Color sky = skyDayColor.Evaluate(n_time);
-        Color fog = fogDayColor.Evaluate(n_time);
-        skybox.SetColor("_SkyColor", sky);
-        skybox.SetColor("_FogColor", fog);
-    }
-
-    private void UpdateSkyNightColor()
-    {
-        Color sky = skyNightColor.Evaluate(n_time);
-        Color fog = fogNightColor.Evaluate(n_time);
-        skybox.SetColor("_SkyColor", sky);
-        skybox.SetColor("_FogColor", fog);
+        float moonAngle = (timeOfDay - 0.5f) * 360.0f - 90.0f;
+        moon.transform.localRotation = Quaternion.Euler(moonAngle, 0.0f, 0.0f);
+        moon.intensity = Vector3.Dot(moon.transform.forward, Vector3.down) * moonIntensityFactor;
+        moon.intensity = Mathf.Max(moon.intensity, 0.0f);
     }
 }
