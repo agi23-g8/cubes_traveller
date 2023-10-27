beef2337d9f2b61d0561e39a941da09561be99aa
Anders Blomqvist <anders@minaemail.se>
2023-10-27 13:02:08 +0200

    working cube gravity for ball

--- Assets/Scripts/World/CubeGravity.cs
+++ Assets/Scripts/World/CubeGravity.cs
@@ -1,10 +1,11 @@
-using System.Net.NetworkInformation;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 [ExecuteAlways]
 public class CubeGravity : MonoBehaviour
 {
+    public float gravityMultplier = 1.0f;
+
     [Header("Main Cube transform")]
     public Transform cubeTransform;
 
@@ -33,15 +34,18 @@ public class CubeGravity : MonoBehaviour
         Vector3 rayDir = cubeTransform.position - transform.position;
         Vector3 currentNormal = transform.up;
 
+        Debug.DrawRay(transform.position, rayDir, Color.blue);
+
         RaycastHit hit;
         if (Physics.Raycast(transform.position, rayDir, out hit, Mathf.Infinity, layerMask))
         {
-            currentNormal = hit.normal + hit.point;
-            Debug.DrawLine(hit.normal + hit.point, hit.point, Color.green);
+            currentNormal = hit.normal;
+            Debug.DrawRay(hit.point, currentNormal, Color.green);
         }
 
-        rb.AddForce(currentNormal * -9.81f, ForceMode.Acceleration);
+        currentNormal *= -1;
+        rb.AddForce(currentNormal * gravityMultplier, ForceMode.Force);
 
-        Debug.DrawRay(transform.position, transform.position + currentNormal * -9.81f, Color.red);
+        Debug.DrawRay(transform.position, currentNormal, Color.red);
     }
 }
